9b84190ca14fa1b7d9dc0e439492081e2a61722f
mina-meowmeow <146037956+mina-meowmeow@users.noreply.github.com>
2024-07-03 00:45:33 +0700

    Create practice_1.sql

---
+++ practice_1.sql
@@ -0,0 +1,12 @@
+---ex1
+select name from city 
+where countrycode='USA' and population > 120000;
+
+---ex2
+select * from city
+where countrycode = 'JPN';
+
+---ex3
+select city, state from station;
+
+---ex4
